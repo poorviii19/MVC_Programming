5c7ea9faefd1b181d725f6aeeb1e5043ae67480d
poorviii19 <shuklapoorviswe@gmail.com>
2026-06-08 18:17:03 +0530

    assets url

---
+++ resources/views/url.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+    <img src="{{ asset('images/logo.png') }}" alt="Logo">
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+</html>
